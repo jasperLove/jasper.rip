4dee55ce4f32876205893d489f6b119dc8d93cfc
jasper <jasper@priest.com>
2022-12-02 22:45:40 +1030

    adding bigwigs and plater bug fixes.

---
+++ lua/BigWigs.lua
@@ -0,0 +1,113 @@
+
+BigWigs3DB = {
+	["namespaces"] = {
+		["BigWigs_Plugins_AltPower"] = {
+			["profiles"] = {
+				["jasper.rip"] = {
+					["disabled"] = true,
+					["fontName"] = "Friz Quadrata TT",
+				},
+			},
+		},
+		["BigWigs_Plugins_Wipe"] = {
+		},
+		["BigWigs_Plugins_Colors"] = {
+		},
+		["BigWigs_Plugins_Raid Icons"] = {
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["BigWigs_Plugins_Bars"] = {
+			["profiles"] = {
+				["Default"] = {
+					["BigWigsAnchor_width"] = 220.0000610351563,
+					["BigWigsEmphasizeAnchor_height"] = 21.99996566772461,
+					["BigWigsAnchor_height"] = 15.99998760223389,
+					["BigWigsEmphasizeAnchor_width"] = 320.0000305175781,
+				},
+				["jasper.rip"] = {
+					["outline"] = "OUTLINE",
+					["fontName"] = "Friz Quadrata TT",
+				},
+			},
+		},
+		["BigWigs_Plugins_Victory"] = {
+		},
+		["BigWigs_Plugins_Statistics"] = {
+		},
+		["BigWigs_Plugins_Sounds"] = {
+		},
+		["BigWigs_Plugins_InfoBox"] = {
+		},
+		["BigWigs_Plugins_BossBlock"] = {
+		},
+		["BigWigs_Plugins_AutoReply"] = {
+		},
+		["BigWigs_Plugins_Messages"] = {
+			["profiles"] = {
+				["jasper.rip"] = {
+					["outline"] = "OUTLINE",
+					["emphFontName"] = "Friz Quadrata TT",
+					["emphUppercase"] = false,
+					["fontName"] = "Friz Quadrata TT",
+					["emphOutline"] = "OUTLINE",
+					["normalPosition"] = {
+						nil, -- [1]
+						nil, -- [2]
+						nil, -- [3]
+						-315, -- [4]
+					},
+				},
+			},
+		},
+		["BigWigs_Plugins_Proximity"] = {
+			["profiles"] = {
+				["jasper.rip"] = {
+					["width"] = 139.9999847412109,
+					["disabled"] = true,
+					["height"] = 120.0000076293945,
+				},
+			},
+		},
+		["BigWigs_Plugins_Pull"] = {
+			["profiles"] = {
+				["jasper.rip"] = {
+					["voice"] = "English: Overwatch: Mei",
+				},
+			},
+		},
+		["BigWigs_Plugins_Countdown"] = {
+			["profiles"] = {
+				["jasper.rip"] = {
+					["outline"] = "OUTLINE",
+					["fontSize"] = 70,
+					["fontColor"] = {
+						["g"] = 1,
+						["b"] = 1,
+					},
+					["fontName"] = "Friz Quadrata TT",
+					["position"] = {
+						nil, -- [1]
+						nil, -- [2]
+						nil, -- [3]
+						-400, -- [4]
+					},
+					["voice"] = "English: Overwatch: Mei",
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Yay - Jubei'Thos"] = "jasper.rip",
+	},
+	["profiles"] = {
+		["Default"] = {
+		},
+		["jasper.rip"] = {
+		},
+	},
+}
+BigWigsIconDB = {
+}
+BigWigsStatsDB = {
+}
